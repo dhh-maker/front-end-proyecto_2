f769ce073804e2bb4058bd0a000110a2c80656ba
AusDr4ko! <diego0hh013@gmail.com>
2026-05-05 13:44:29 +0000

    HERO BETA

--- front-end-proyecto-equipo08/app/page.tsx
+++ front-end-proyecto-equipo08/app/page.tsx
@@ -1,11 +1,13 @@
 import Steps from "./Components/Steps/Steps";
 import CardVacante from "./Components/CardVacante/CardVacante";
 import Header from './Components/Header/Header';
+import Hero from './Components/Hero/Hero';
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Header/>
+      <Hero />
       <CardVacante />
       <main>
         <Steps />
