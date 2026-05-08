87c75626f3e99d89ef8c034859da7321e9080f12
mauriciobaezv <mauricio.baez.villanueva@gmail.com>
2026-05-08 03:52:08 +0000

    feat: mejora visual Steps cards con hover, glassmorphism y links

--- front-end-proyecto-equipo08/app/page.tsx
+++ front-end-proyecto-equipo08/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
     <>
       <Carousel/>
       <Hero />
-      <CardVacante />
       <main>
         <Steps />
       </main>
+      <CardVacante />
     </>
   );
 }
